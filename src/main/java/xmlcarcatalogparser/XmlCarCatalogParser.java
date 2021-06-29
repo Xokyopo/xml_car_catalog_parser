8435c7d3cfea7366a4ee4c9e920b9a6cc03469d2
Aleksei Shorokhov <enslaver@list.ru>
2021-06-29 16:51:08 +0300

    add XmlCarCatalogParser class

---
+++ src/main/java/xmlcarcatalogparser/XmlCarCatalogParser.java
@@ -0,0 +1,41 @@
+package xmlcarcatalogparser;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class XmlCarCatalogParser {
+    private static final ParserTasks PARSER_TASKS = new ParserTasks();
+
+    public static void main(String[] args) {
+        (new XmlCarCatalogParser()).run(args);
+    }
+
+    private String extractFileName(String[] args) {
+        if (args == null || args.length < 1) {
+            throw new IllegalArgumentException(
+                    "Command line Argument not found. I wait 1 argument as file name template:{appName youFileName}");
+        }
+
+        String fileName = args[0];
+
+        if (Files.notExists(Path.of(fileName)))
+            throw new IllegalArgumentException(String.format("File {%s} not fount", fileName));
+
+        if (!Files.isReadable(Path.of(fileName)))
+            throw new UncheckedAccessDeniedException(String.format("Cant permission wor read file {%s}", fileName));
+
+        return fileName;
+    }
+
+    public void run(String[] args) {
+        try {
+            String fileName = this.extractFileName(args);
+
+            System.out.println(PARSER_TASKS.countUniqueProperty(fileName, "modification", "name"));
+
+        } catch (Exception e) {
+            System.err.println("Application terminated with error");
+            e.printStackTrace();
+        }
+    }
+}

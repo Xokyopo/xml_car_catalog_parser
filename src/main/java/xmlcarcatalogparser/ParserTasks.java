0381dd062e0860626d115b51f01a7b40db5cce8b
Aleksei Shorokhov <enslaver@list.ru>
2021-06-29 22:50:14 +0300

    update method countUniqueProperty id ParserTasks -add collapsing whitespace

--- src/main/java/xmlcarcatalogparser/ParserTasks.java
+++ src/main/java/xmlcarcatalogparser/ParserTasks.java
@@ -6,17 +6,16 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 public class ParserTasks {
     public int countUniqueProperty(String fileName, String tagName, String attributeName) throws ParseTaskException {
         Set<String> attributesNames = new HashSet<>();
-        AtomicInteger counter = new AtomicInteger(0);
 
         Consumer<String> ifFountCallback = (attributeValue) -> {
+            attributeValue = attributeValue.replace("[ ]*", " ");
+            attributeValue = attributeValue.trim();
             attributesNames.add(attributeValue);
-            counter.incrementAndGet();
         };
 
         this.parse(fileName, new TagAttributePropertyInterceptor(tagName, attributeName, ifFountCallback));
